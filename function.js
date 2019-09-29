ca521830cbb9c17828015ac736f71da591cdc940
alodor <jose-a-mendez@hotmail.com>
2019-09-28 21:44:01 -0400

    Creación de funciones para configrar la fecha y el intervalo de tiempo

--- function.js
+++ function.js
@@ -1,16 +1,97 @@
-const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24
+$(document).ready(() => {
+    $('#accept').click(() => {
+        
+        let date = $('#date').val()
+        let dateNew = date.split('-')
+        
+        let year = dateNew[0]
+        let getMonth = dateNew[1]
+        let days = dateNew[2]        
+        
+        let month = setMonth()
+        
+        // Choose month with conditional
+        function setMonth() {
+            switch(getMonth) {
+                case '01':
+                    return 'Jan'
+                    break
+                
+                case '02':
+                    return 'Feb'
+                    break
+                    
+                case '03':
+                    return 'Mar'
+                    break
+                    
+                case '04':
+                    return 'Apr'
+                    break
+                    
+                case '05':
+                    return 'May'
+                    break
+                    
+                case '06':
+                    return 'Jun'
+                    break
+                    
+                case '07':
+                    return 'Jul'
+                    break
+                    
+                case '08':
+                    return 'Aug'
+                    break
+                    
+                case '09':
+                    return 'Sep'
+                    break
+                    
+                case '10':
+                    return 'Oct'
+                    break
+                    
+                case '11':
+                    return 'Nov'
+                    break
+                    
+                default:
+                    return 'Dec'
+            }
+        }   
+        
+        // Launch function
+        let getDateTime = setDate(days, month, year)        
+        config_interval(getDateTime)
+    })    
+})
 
-let countDown = new Date('Mar 24, 2020 00:00:00').getTime(),
-    x = setInterval(function() {
 
-        let now = new Date().getTime(),
-            distance = countDown - now
+// Setting Date
+function setDate(days, month, year) {
+    
+    let getDateTime = new Date(month + ' ' + days + ', ' + year + ' 00:00:00').getTime()    
+    return getDateTime
+}
+
+
+// Setting Interval
+function config_interval(getDateTime) {
+    
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24
+    
+    let interval = setInterval(() => {
+    
+        let now = new Date().getTime()
+        let distance = getDateTime - now
 
         if (distance < 0) {
-           clearInterval(x)
+           clearInterval(interval)
         }
 
         document.getElementById('days').innerText = Math.floor(distance / (day)),
@@ -18,4 +99,7 @@ let countDown = new Date('Mar 24, 2020 00:00:00').getTime(),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second)
 
-    }, second)
+    }, second)   
+    
+    return interval
+}
